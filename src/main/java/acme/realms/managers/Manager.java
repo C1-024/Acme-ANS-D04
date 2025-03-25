35a80597c755c16f5cb115e7f7a454f5e42f03de
David Vargas Muñiz <davidcolegalaroza@gmail.com>
2025-03-25 15:06:55 +0100

    fix: Several errors fixed

--- src/main/java/acme/realms/managers/Manager.java
+++ src/main/java/acme/realms/managers/Manager.java
@@ -17,14 +17,13 @@ import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidUrl;
-import acme.constraints.ValidManager;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-@ValidManager
+//@ValidManager
 public class Manager extends AbstractRole {
 
 	// Serialisation version ---------------------------------------------------------------------------------------
